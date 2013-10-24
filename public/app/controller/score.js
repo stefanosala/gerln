edd83917fc8335ce017e370edfa9bc1a1d9336a8
Stefano Sala <stefano.sala@spa.it>
2013-10-24 15:18:14 +0200

    Added highscores

--- public/app/controller/score.js
+++ public/app/controller/score.js
@@ -1,10 +1,18 @@
 var ScoreController = angular.module('ScoreController', []);
 
-ScoreController.controller('ScoreController', ['$scope', '$location', '$routeParams',
-    function ScoreController($scope, $location, $routeParams) {
+ScoreController.controller('ScoreController', ['$scope', '$location', '$routeParams', 'HighscoresRepository',
+    function ScoreController($scope, $location, $routeParams, HighscoresRepository) {
         $scope.seconds = Math.floor((new Date() - new Date(parseInt($routeParams.startedAt, 10))) / 1000);
 
+        $scope.highscores = HighscoresRepository.ranking(5);
+
         $scope.restart = function () {
+            $location.path('/');
+        };
+
+        $scope.clear = function () {
+            HighscoresRepository.clear();
+
             $location.path('/');
         };
     }]
--- public/app/repository/highscores.js
+++ public/app/repository/highscores.js
@@ -19,6 +19,27 @@ HighscoresRepository.factory('HighscoresRepository', [
                 if (highscore > 0) {
                     localStorage.setItem(word.german, highscore - 1);
                 }
+            },
+            ranking: function (limit) {
+                var words = [];
+
+                for (var key in localStorage) {
+                    words.push({
+                        word: key,
+                        value: parseInt(localStorage.getItem(key), 10)
+                    });
+                }
+
+                return words.sort(function (a, b) {
+                    if (a.value === b.value) {
+                        return 0;
+                    }
+
+                    return a.value > b.value ? -1 : 1;
+                }).slice(0, limit);
+            },
+            clear: function () {
+                localStorage.clear();
             }
         };
     }
